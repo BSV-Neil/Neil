bee754ab9a0934b4fd7cdf0e0a43e500613bed31
NEIL <NEILFORD21@GMAIL.COM>
2019-05-20 21:07:19 -0700

    commit all

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ if (usersAge === '33') {
 } else {
     document.write('<h2>LIAR!</h2>');
 }
+
+'use strict'; // make our linter happy
+
+// inputs (parameters) - pants, shirt, hat
+
+funtion getDressed (pants, shirt, hat) {
+    var getDressed = 'you have on' +pants+ and + shirt + with a +hat+ on;
+    
+    // using that string, construct a bit of html that can be placed in the site
+    var clothingOnHtml = '<p>' + getDressed + '<p>';
+
+    //now, set the outputs
+    return clothingOnHtml;
+
+    //prompt next adult what they want to wear
+    var kindofPants = prompt('what kind of pants do you want to wear?');
+    var kindofShirt = prompt('what color of shirt would you like to wear?');
+    var kindofHat = prompt('what kind of hat would you like to wear?');
+
+    var dressing = getDressed(kindofPants, kindofShirt, kindofHat)
+
+// thenn make that bit of html show up in the site
+
+document.write dressing;
+
+var = change = prompt('do you want to change?');
+
+if (change === 'yes') {
+    // prompt the user for a style change
+    var kindofPants2 = prompt('what kind of pants do you want to wear?');
+    var kindofShirt2 = prompt('what color of shirt would you like to wear?');
+    var kindofHat2 = prompt('what kind of hat would you like to wear?');
+
+    var dressingtwo = getDressed(kindofPants, kindofShirt, kindofHat)
+
+    // then, make that bit of html show up in the site
+    document.write(dressingtwo);
+}
+}
